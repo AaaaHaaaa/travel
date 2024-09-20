c708f91f8be84febe83d67ae9d58f574d227e1eb
AaaaHaaaa <1207062208@qq.com>
2024-09-20 17:34:50 +0800

    day1

---
+++ Vue/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="main">
+    <el-container style="width: 100%; height: 100%">
+      <el-aside>
+        <Aside :talks="talks" />
+      </el-aside>
+      <el-container style="width: 75%; height: 100%">
+        <el-main>Main</el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed, reactive } from "vue";
+import Aside from "./components/aside.vue";
+
+const talks = reactive([
+  { id: 1, name: "FirstTalk" },
+  { id: 2, name: "SecondTalk" },
+  { id: 3, name: "ThirdTalk" },
+]);
+</script>
+
+<style scoped>
+.main {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+.el-aside {
+  background-color: #eae9f4;
+}
+.el-main {
+  height: 70%;
+}
+.el-footer {
+  height: 30%;
+}
+</style>
---
+++ Vue/src/components/aside.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="create">
+    <div>
+      <el-button
+        color="#626aef"
+        :dark="isDark"
+        plain
+        round
+        style="width: 180px; height: 50px"
+      >
+        + 新建对话
+      </el-button>
+    </div>
+    <div class="list">
+      <h5 v-if="talks == {}">暂无数据</h5>
+      <ul class="talks-list">
+        <li v-for="item in talks" :key="item.id">
+          <div class="talks-view">
+            <p>{{ item.name }}</p>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script  setup>
+const talks = defineProps(["talks"]);
+console.log(talks.value);
+</script>
+
+<style scoped>
+.create {
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+  grid-template-rows: 1fr auto;
+}
+.list {
+  margin: 0 auto;
+  margin-top: 50px;
+}
+</style>
